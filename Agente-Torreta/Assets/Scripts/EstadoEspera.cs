dfcf36b80684ed6c1c272b700112d9cb0e00166f
Curicaueri <80057552+Curicaueri@users.noreply.github.com>
2021-03-11 10:54:26 -0600

    Estado de espera y Torreta estatica
    Añadido de más scripts

---
+++ Agente-Torreta/Assets/Scripts/EstadoEspera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstadoEspera : InstruccionesFSM
+{
+
+    public override void Start(TorretaEstatica torreta)
+    {
+        //inicia el estado de espera
+    }
+
+
+    public override void UpdateState(TorretaEstatica torreta)
+    {
+        //ejecición del estado de espera
+    }
+}
--- Agente-Torreta/Assets/Scripts/InstruccionesFSM.cs
+++ Agente-Torreta/Assets/Scripts/InstruccionesFSM.cs
@@ -2,7 +2,7 @@
 
 public abstract class InstruccionesFSM
 {
-    public abstract void EnterState();
+    public abstract void EnterState(TorretaEstatica torreta);
     
-    public abstract void UpdateState();
+    public abstract void UpdateState(TorretaEstatica torreta);
 }
