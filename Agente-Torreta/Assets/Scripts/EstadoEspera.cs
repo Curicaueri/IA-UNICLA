89f9bc9f3649e993afa7060b83d8763280b0d7b2
Curicaueri <80057552+Curicaueri@users.noreply.github.com>
2021-03-18 10:59:17 -0600

    Bullet and new states
    Added Bullet and new states

--- Agente-Torreta/Assets/Scripts/EstadoEspera.cs
+++ Agente-Torreta/Assets/Scripts/EstadoEspera.cs
@@ -1,19 +1,31 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EstadoEspera : InstruccionesFSM
 {
-
-    public override void Start(TorretaEstatica torreta)
+    public override void EnterState(TorretaEstatica torreta)
     {
-        //inicia el estado de espera
         Debug.Log("Inicio Estado de espera!");
+        torreta.currentState = AgentState.Idle;
+        torreta.Coroutine(Wait(torreta));
     }
 
+    IEnumerator Wait(TorretaEstatica agent)
+    {
+        Debug.Log("Inicio conteo");
+        yield return new WaitForSeconds(agent.timeIdle);
+        ///Se ejecuta despues de X segundos
+        Debug.Log("Cambio a estado de rotar!");
+        agent.TransitionToState(agent.rotateState);
+    }
 
     public override void UpdateState(TorretaEstatica torreta)
     {
-        //ejecución del estado de espera
+        if(torreta.playerDetected == true)
+        {
+            torreta.StopAllCoroutines();
+            torreta.TransitionToState(torreta.attackState);
+        }
     }
 }
--- Agente-Torreta/Assets/Scripts/EstadoRotacion.cs
+++ Agente-Torreta/Assets/Scripts/EstadoRotacion.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +6,28 @@ public class EstadoRotacion : InstruccionesFSM
 {
     public override void EnterState(TorretaEstatica torreta)
     {
-        
+        Debug.Log("Entro a estado de rotación");
+        torreta.currentState = AgentState.Rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void UpdateState(TorretaEstatica torreta)
     {
+        if (torreta.playerDetected == true)
+        {
+            torreta.TransitionToState(torreta.attackState);
+        }
+        else
+        {
+            torreta.transform.rotation = Quaternion.Slerp(torreta.transform.rotation,
+        Quaternion.Euler(torreta.angles[torreta.angleIndex]), Time.deltaTime * torreta.speedRotation);
+
+            if (torreta.transform.eulerAngles.y >= (torreta.angles[torreta.angleIndex].y - 1))
+            {
+                Debug.Log("Llegue al angulo destino");
+                torreta.angleIndex = (torreta.angleIndex + 1) % torreta.angles.Length;
+                torreta.TransitionToState(torreta.idleState);
+            }
+        }
         
     }
 }
